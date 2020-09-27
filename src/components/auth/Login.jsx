a4d04966daa8183eeafbe83dfc93a645a890ef68
bushbass <alex.nielsen@gmail.com>
2020-09-26 22:16:29 -0400

    finished video 7

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,33 +2,41 @@ import React, { useState, useContext } from 'react';
 import Axios from 'axios';
 import UserContext from '../../context/UserContext';
 import { useHistory } from 'react-router-dom';
+import ErrorNotice from '../misc/ErrorNotice';
 
 function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-
+  const [error, setError] = useState();
   const { setUserData } = useContext(UserContext);
   const history = useHistory();
 
   const submit = async (e) => {
     e.preventDefault();
-    const loginUser = { email, password };
-    await Axios.post('http://localhost:5000/users/login', loginUser);
-    const loginResponse = await Axios.post(
-      'http://localhost:5000/users/login',
-      { email, password }
-    );
-    setUserData({
-      token: loginResponse.data.token,
-      user: loginResponse.data.user,
-    });
-    localStorage.setItem('auth-token', loginResponse.data.token);
-    history.push('/');
+    try {
+      const loginUser = { email, password };
+      await Axios.post('http://localhost:5000/users/login', loginUser);
+      const loginResponse = await Axios.post(
+        'http://localhost:5000/users/login',
+        { email, password }
+      );
+      setUserData({
+        token: loginResponse.data.token,
+        user: loginResponse.data.user,
+      });
+      localStorage.setItem('auth-token', loginResponse.data.token);
+      history.push('/');
+    } catch (err) {
+      err.response.data.msg && setError(err.response.data.msg);
+    }
   };
 
   return (
     <div className='page'>
       <h2>Login</h2>
+      {error && (
+        <ErrorNotice message={error} clearError={() => setError(undefined)} />
+      )}
       <form className='form' onSubmit={submit}>
         <label htmlFor='login-email'>Email</label>
         <input
