c3322c09edce6c21d5ca6dba8226f0912c96aa29
Alex Nielsen <alex@bigal.org>
2020-09-28 18:36:41 -0400

    fixed so logout on todos page redirects to home

--- src/components/pages/UserDisplay.jsx
+++ src/components/pages/UserDisplay.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useContext } from 'react';
 import Axios from 'axios';
 import UserContext from '../../context/UserContext';
+import { useHistory } from 'react-router-dom';
 
 function UserDisplay() {
   const [todos, setTodos] = useState([]);
-  const { BACKEND_URL } = useContext(UserContext);
+  const { BACKEND_URL, userData } = useContext(UserContext);
+  const history = useHistory();
 
   useEffect(() => {
     async function fetchData() {
@@ -19,6 +21,10 @@ function UserDisplay() {
     }
     fetchData();
   }, [BACKEND_URL]); // Or [] if effect doesn't need props or state
+
+  useEffect(() => {
+    if (!userData.user) history.push('/login');
+  });
 
   return (
     <div>
